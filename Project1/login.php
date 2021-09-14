a603ac79092735ae69c2610463b83a6c7e8cd1d8
Rufrufina27 <agnesstefani30@gmail.com>
2021-09-14 08:54:22 +0700

    tampilan login

--- Project1/login.php
+++ Project1/login.php
@@ -30,15 +30,21 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/484db9065f.js" crossorigin="anonymous"></script>
     <title>Login</title>
 </head>
 
 <body class="masuk">
     <div class="container">
+        <div class="header">
+            <img src="Logonobg.png">
+        </div>
     <form class="form-horizontal" id="form" name="myForm" method="POST">
+
         <div class="form-group">
         <label class="control-label col-sm-2" for="username">Username</label>
-        <div class="col-sm-10">
+        <div class="col-sm-10" style="text-align: center;">
             <input value="<?php if (isset($_POST['username'])) echo htmlspecialchars($_POST['username']) ?>" type="text" name="username" class="form-control" id="username" placeholder="Enter username" required/>
             <div class="invalid-feedback">Please fill out this field.</div>
         </div>
@@ -52,7 +58,7 @@
         </div>
         <div class="form-group">        
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-primary" value="Login" name="login">Submit</button>
+            <button style="margin-bottom: 45px" type="submit" class="btn btn-primary" value="Login" name="login">Login</button>
         </div>
         </div>
     </form>

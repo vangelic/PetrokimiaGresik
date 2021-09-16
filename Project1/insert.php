11e3f3c8e9a166440454c1b7945d5df8a572d696
vangelic <ivfatusy@gmail.com>
2021-09-16 14:04:05 +0700

    form

--- Project1/insert.php
+++ Project1/insert.php
@@ -1,50 +1,43 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://kit.fontawesome.com/484db9065f.js" crossorigin="anonymous"></script>
+	<title>Insert Data</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
 </head>
+
 <body>
-	<thead>
-		<div class="logo">
-			<img src="Logonobg.png" width="140px" height="50px">
-				<ul class="home">
-					<a href="home.php" style="margin-right: 30px">Home</a>
-					<a href="index.php">Logout</a>
-				</ul>
-		</div>
-	</thead>
-	<tbody>
-		<div class="container">
-		<div class="header">
-			<h1>INSERT DATA</h1>
-		</div>
-		<div class="main">
-			<form>
-				<h3>Nama Lokal</h3>
-				<span>
-					<input type="text" placeholder="Nama Lokal" name="">
-				</span><br>
-				<h3>Nama Latin</h3>
-				<span>
-					<input type="text" placeholder="Nama Latin" name="">
-				</span><br>
-				<h3>Deskripsi</h3>
-				<span>
-					<input type="text" placeholder="Deskripsi" name="">
-				</span><br>
-				<h3>File Browser</h3>
-				<span>
-					<input type="File" placeholder="File Browser" name="">
-				</span><br>
-				<button style="margin-top: 50px;">INSERT</button>
-			</form>
-		</div>
+	<div class="container">
+		<h2>Form</h2>
+		<hr style="position: relative; border: none; height: 1px; background: #999;" />
+		<form class="row g-3" id="form" name="myForm" method="POST">
+			<div class="col-md-6">
+				<div class="mb-3">
+					<label for="nm_lokal" class="form-label">Nama Lokal</label>
+    				<input type="text" class="form-control" id="nm_lokal" placeholder="Masukkan nama lokal">
+				</div>
+				<div class="mb-3">
+					<label for="nm_latin" class="form-label">Nama Latin</label>
+    				<input type="text" class="form-control" id="nm_latin" placeholder="Masukkan nama latin">
+				</div>
+				<div class="mb-3">
+					<label for="deskripsi" class="form-label">Deskripsi</label>
+    				<textarea class="form-control" id="deskripsi" placeholder="Masukkan deskripsi" rows="3"></textarea>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="mb-3">
+					<label for="formFile" class="form-label">Upload Foto</label>
+					<input class="form-control" type="file" id="formFile">
+				</div>
+			</div>	
+			<div class="text-end">
+				<button type="submit" name="upload" value="Upload" class="btn btn-success">Submit</button>
+			</div>		
+		</form>
 	</div>
-	</tbody>
 </body>
-<footer style="text-align: center; width: 100%">
-	<img src="kontak.png" height="30px">
-</footer>
 </html>
--- Project1/login.php
+++ Project1/login.php
@@ -27,43 +27,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://kit.fontawesome.com/484db9065f.js" crossorigin="anonymous"></script>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <title>Login</title>
 </head>
 
-<body class="masuk">
+<body>
     <div class="container">
-        <div class="header">
-            <img src="Logonobg.png">
-        </div>
-    <form class="form-horizontal" id="form" name="myForm" method="POST">
-
-        <div class="form-group">
-        <label class="control-label col-sm-2" for="username"><b>Username</b></label>
-        <div class="col-md-4 inputGroupContainer">
-            <div class="input-group">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                <input value="<?php if (isset($_POST['username'])) echo htmlspecialchars($_POST['username']) ?>" type="text" name="username" class="form-control" id="username" placeholder="Enter username" required/>
-                <div class="invalid-feedback">Please fill out this field.</div>
+        <form class="row g-3" id="form" name="myForm" method="POST">
+        <div class="col-12">
+            <label for="username" class="form-label">Username</label>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
+                <input value="<?php if (isset($_POST['username'])) echo htmlspecialchars($_POST['username']) ?>" name="username" type="text" class="form-control" id="username" placeholder="Masukkan username" required>
             </div>
         </div>
-        </div>
-        <div class="form-group">
-        <label class="control-label col-sm-2" for="passwd"><b>Password</b></label>
-        <div class="col-md-4 inputGroupContainer">
-            <div class="input-group">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                <input value="<?php if (isset($_POST['passwd'])) echo htmlspecialchars($_POST['passwd']) ?>" type="password" class="form-control" id="passwd" placeholder="Enter password" name="passwd" required />
-            <div class="invalid-feedback">Please fill out this field.</div>
+        <div class="col-12">
+            <label for="passwd" class="form-label">Password</label>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
+                <input value="<?php if (isset($_POST['passwd'])) echo htmlspecialchars($_POST['passwd']) ?>" type="password" name="passwd" class="form-control" id="passwd" placeholder="Masukkan password" required>
             </div>
         </div>
+        <div class="col-12">
+            <button type="submit" name="login" value="Login" class="btn btn-success">Login</button>
         </div>
-        <div class="form-group" style="text-align: center;">        
-            <button style="margin-bottom: 45px" type="submit" class="btn btn-success" value="Login" name="login">Login</button>
-        </div>
-    </form>
+        </form>
     </div>
 </body>
 

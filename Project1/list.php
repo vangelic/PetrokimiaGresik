e8d4cdc58eaa812bf0cbbc757912e2b6694f315d
vangelic <ivfatusy@gmail.com>
2021-09-20 22:50:46 +0700

    list

--- Project1/list.php
+++ Project1/list.php
@@ -25,12 +25,10 @@ $result = mysqli_query($koneksi, "SELECT * FROM pgpedia");
 			background-repeat: no-repeat;
 			background-size: cover;
 		}
-		.container{
-			width: 480px;
-			margin: 5% auto;
-			border-radius: 25px;
-			background-color: rgba(255,255,255,0.5);
-			box-shadow: 0 0 17px #333;
+		.container-fluid{
+    	border-radius: 25px;
+    	background-color: rgba(255,255,255,0.8);
+    	box-shadow: 0 0 17px #333;
 		}
 		.home{
 			float: right;
@@ -45,11 +43,14 @@ $result = mysqli_query($koneksi, "SELECT * FROM pgpedia");
 		}
 
 	</style>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </head>
 <body>
 	<thead>
 		<div class="logo">
-			<img src="Logonobg.png" width="140px" height="50px">
+			<img src="aset/Logonobg.png" width="140px" height="50px">
 				<ul class="home">
 					<a href="insert.php" style="margin-right: 30px">Home</a>
 					<a href="logout.php">Logout</a>
@@ -57,10 +58,11 @@ $result = mysqli_query($koneksi, "SELECT * FROM pgpedia");
 		</div>
 	</thead>
 	<tbody>
-		<div class="container">
+		<div class="container-fluid" style="margin: 50px; padding:30px;
+	width: calc(100% - 100px);">
 		<h2>Daftar Tanaman</h2>
 		<hr style="position: relative; border: none; height: 1px; background: #999;" />
-			<table border="1" cellpadding="10" cellspacing="0">
+		<table class="table text-center align-middle">
 				<tr>
 					<th>No.</th>
 					<th>Aksi</th>

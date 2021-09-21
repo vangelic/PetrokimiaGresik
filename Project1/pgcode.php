769f73de2fd4fc8635f34b9f7073b4d256eabe2c
vangelic <ivfatusy@gmail.com>
2021-09-21 14:56:46 +0700

    yg3

--- Project1/pgcode.php
+++ Project1/pgcode.php
@@ -28,7 +28,7 @@
 		<hr style="position: relative; border: none; height: 1px; background: #999;" />
 		<div class="row">
             <div class="col-md-6 text-center">
-                <img src="pgqrcode/qr<?php echo $c_id ?>.png" id="pg_code" class="img-thumbnail" alt="qrcode" style="width:500px;">
+                <img src="pgqrcode/qr<?php echo $c_id ?>.png" id="pg_code" class="img-thumbnail" alt="qrcode" style="width:300px;">
             </div>
             <div class="col-md-6 text-center d-flex flex-column justify-content-center align-items-center">
                 <p>Klik Tombol dibawah untuk Mengunduh QR Code :</p>

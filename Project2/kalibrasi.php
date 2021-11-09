bdc43f34026738d3e1903ae86437623e631e89ee
vangelic <ivfatusy@gmail.com>
2021-11-09 15:07:07 +0700

    Update kalibrasi.php

--- Project2/kalibrasi.php
+++ Project2/kalibrasi.php
@@ -12,7 +12,7 @@
 			$statement->bindValue(':alat', $_POST['alat']);
 			$statement->bindValue(':id', $date);
 			
-			$statement->execute() or die ('Error '.$statement->errorInfo()[2]);
+			$statement->execute() or die ('Error '.$statement->errorInfo()[2].$_POST['date']);
 
 		header("Location: $url/admin.php");
         exit();

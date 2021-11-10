a356020667a5efb52a1c9f5a214f7072e37d3e27
Rufrufina27 <agnesstefani30@gmail.com>
2021-11-10 15:39:11 +0700

    mobile interface

--- Project2/login.php
+++ Project2/login.php
@@ -44,6 +44,54 @@
         font-size: 16px;
         top: 10px;
     }
+    @media screen and (max-width: 768px){
+        .container{
+            width: auto;
+            height: 700px;
+        }
+    }
+    @media screen and (max-width: 320px){
+        .container{
+            width: 300px !important;
+            height: 525px;
+        }
+        .header img{
+            width: 250px;
+            height: 120px;
+        }
+        .col-12 button{
+            width: 200px;
+            margin-bottom: 20px;
+        }
+    }
+    @media screen and (max-width: 375px){
+        .container{
+            width: 350px !important;
+            height: 525px;
+        }
+        .header img{
+            width: 300px;
+            height: 120px;
+        }
+        .col-12 button{
+            width: 200px;
+            margin-bottom: 20px;
+        }
+    }
+    @media screen and (max-width: 425px){
+        .container{
+            width: 350px !important;
+            height: 525px;
+        }
+        .header img{
+            width: 300px;
+            height: 120px;
+        }
+        .col-12 button{
+            width: 200px;
+            margin-bottom: 20px;
+        }
+    }
 </style>
 
 <body style="background-image: url(aset/bg.jpg)">

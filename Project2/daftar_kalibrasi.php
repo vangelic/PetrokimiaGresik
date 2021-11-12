cec1901924d1779cd84c6a556b81ce80aee5d153
vangelic <ivfatusy@gmail.com>
2021-11-12 11:22:15 +0700

    Update daftar_kalibrasi.php

--- Project2/daftar_kalibrasi.php
+++ Project2/daftar_kalibrasi.php
@@ -2,14 +2,6 @@
 	include "inisiasi.php";
 	require 'adminPermission.inc.php';
 
-	if (isset($_POST['hapus'])) {
-
-		$db->delete('kalibrasi', ['id_kalibrasi' => $row['id_kalibrasi']]);
-
-		header("Location: $url/user.php");
-        exit();
-    }
-
 	$result = mysqli_query($koneksi, "SELECT id_kalibrasi, nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat ORDER BY tgl_kalibrasi ASC");
 ?>
 
@@ -92,6 +84,7 @@
 								</div>
 							</div>
 						</td>
+						<td>
 							<button type="button" class="btn btn-primary">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16">
 								<path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
@@ -115,7 +108,7 @@
 
 							$cek = date('Y-m-d', strtotime($kal));
 
-							$interval -> $now -> diff($cek);
+							$interval = $now -> diff($cek);
 								
 							if ($now >= $cek){
 						?>

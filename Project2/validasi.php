97393b9332bd6a3cf4b18713fdc8e2423806ca9c
vangelic <ivfatusy@gmail.com>
2021-11-14 23:20:39 +0700

    Update validasi.php

--- Project2/validasi.php
+++ Project2/validasi.php
@@ -2,9 +2,7 @@
 
 	$id = $_GET['id'];
 
-	$statement = $dbc->prepare("DELETE FROM kalibrasi WHERE id_kalibrasi = :id");
-	$statement->bindValue(':id', $id);
-	$statement->execute() or die ('Error '.$statement->errorInfo()[2]);
+	echo $id;
 
 	header("Location: $url/daftar_kalibrasi.php");
 	exit();

0f4f086c212e6091b8c9a8f295ac2809bb0300cc
vangelic <ivfatusy@gmail.com>
2021-11-15 01:18:42 +0700

    Update user.php

--- Project2/user.php
+++ Project2/user.php
@@ -114,9 +114,10 @@
 							$otherTZ = new DateTimeZone("Asia/Jakarta");
 							$datetime->setTimezone($otherTZ);
 							$date = $datetime->format('Y-m-d');
-							$statement = $dbc->prepare("SELECT nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat AND DATE(kalibrasi.tgl_kalibrasi) >= ? kalibrasi.tgl_kalibrasi ASC");
-							$statement->execute(['2021-11-15']);
-							while ($row = $statement->fetchAll()) {
+							$statement = $dbc->prepare("SELECT nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat AND DATE(kalibrasi.tgl_kalibrasi) >= :date kalibrasi.tgl_kalibrasi ASC");
+							$statement->execute(['date' => $date]);
+							$data = $statement->fetchAll();
+							foreach ($data as $row) {
 								echo "<a href=''><li>{$row['nama_alat']}</li></a>";
 							}
 							?>

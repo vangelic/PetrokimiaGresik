ce4e308d96ff07529462b11036b509e7c3f10e65
vangelic <ivfatusy@gmail.com>
2021-11-14 23:15:53 +0700

    Update daftar_kalibrasi.php

--- Project2/daftar_kalibrasi.php
+++ Project2/daftar_kalibrasi.php
@@ -94,7 +94,9 @@
 										</div>
 									</td>
 									<td>
-										<a href="<?php echo $url ?>/validasi.php?id=<?php $row['id_kalibrasi']; ?>">
+										<?php
+										echo "<a href=".$url."/validasi.php?id=".$row['id_kalibrasi'].">";
+										?>
 											<button type="button" class="btn btn-primary">
 												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16">
 												<path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>

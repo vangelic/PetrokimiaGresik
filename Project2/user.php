a2db8f433d0af052d5584a1cae1c5343b5a5513e
Rufrufina27 <agnesstefani30@gmail.com>
2021-11-17 11:41:00 +0700

    .

--- Project2/user.php
+++ Project2/user.php
@@ -90,12 +90,22 @@
 	nav ul li ul{
 		display: none;
 	}
-	nav ul li ul li{
+	nav ul li ul a{
 		width: 300px;
 		height: auto;
 		background-color: #ffff;
-		border-style: solid !important;
-		text-align: left !important;
+		border-style: solid;
+		text-align: left;
+		font-size: 15px !important;
+		font-style: bold !important;
+	}
+	nav ul li ul p{
+		width: 300px;
+		height: auto;
+		background-color: #ffff;
+		border-style: solid;
+		text-align: left;
+		font-size: 12px !important;
 	}
 </style>
 <body>

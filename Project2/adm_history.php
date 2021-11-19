2e01914f22a4a452b4fcb80f3d6b57076d759010
Rufrufina27 <agnesstefani30@gmail.com>
2021-11-19 15:14:56 +0700

    .

--- Project2/adm_history.php
+++ Project2/adm_history.php
@@ -8,83 +8,96 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>History Admin</title>
-	<style>
-		body{
-			font-family: sans-serif;
-			background-image: url(bg.jpg);
-			background-repeat: no-repeat;
-			background-size: cover;
-		}
-		.container-fluid{
-    	border-radius: 25px;
-    	background-color: rgba(255,255,255,0.8);
-    	box-shadow: 0 0 17px #333;
-		}
-		.home{
-			float: right;
-			display: inline-block;
-			width: 350px;
-			height: 50px;
-			list-style: none;
-			line-height: 50px;	
-			color: black;
-			text-align: center;
-			font-size: 20px;
-		}
-		.home img{
-			width: 30px;
-			height: 30px;
-		}
-		nav{
-			width: 50%;
-			height: 30px;
-			border : 0px solid;
-			line-height: 30px;
-			float: right;
-		}
-		nav ul li{
-			width: 20%;
-			height: 30px;
-			float: left;
-			list-style: none;
-			margin-bottom: 5px;
-		}
-		nav ul li:hover ul{
-			display: block;
-		}
-		nav ul li ul{
-			display: none;
-		}
-		nav ul li ul li{
-			width: 300px;
-			height: 40px;
-			background-color: #ffff;
-			margin-bottom: 2px;
-			border-style: solid !important;
-			text-align: left !important;
-		}
-		.auto {
-		    display:none;
-		    padding:5px;
-		    margin-top:5px;
-		    width:330px;
-		    height:100px;
-		    overflow:auto;
-		}
-		.auto:hover {
-		    display:block;
-		    padding:5px;
-		    margin-top:5px;
-		    width:330px;
-		    height:100px;
-		    overflow:auto;
-		}
-	</style>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  <title>History Admin</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </head>
+<style>
+	body{
+		font-family: sans-serif;
+		background-image: url(bg.jpg);
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	.container{
+		width: auto;
+		margin:5% auto;
+		border-radius: 25px;
+		background-color: rgba(255,255,255,0.5);
+		box-shadow: 0 0 17px #333;
+	}
+	.home{
+		float: right;
+		display: inline-block;
+		width: 350px;
+		height: 50px;
+		list-style: none;
+		line-height: 50px;	
+		color: black;
+		text-align: center;
+		font-size: 20px;
+	}
+	.home img{
+		width: 30px;
+		height: 30px;
+	}
+	nav{
+		width: 50%;
+		height: 30px;
+		border : 0px solid;
+		line-height: 30px;
+		float: right;
+	}
+	nav ul li{
+		width: 20%;
+		height: 30px;
+		float: left;
+		list-style: none;
+		margin-bottom: 5px;
+	}
+	nav ul li:hover ul .auto{
+		display: block;
+		border: 1px solid red;
+        padding:5px;
+        margin-top:5px;
+        width:300px;
+        height:300px;
+        overflow:auto;
+	}
+	nav ul li ul{
+		display: none;
+	}
+	nav ul li ul li{
+		width: 300px;
+		height: 40px;
+		background-color: #ffff;
+		margin-bottom: 2px;
+		border-style: solid !important;
+		text-align: left !important;
+	}
+	.auto {
+	    display:none;
+	    padding:5px;
+	    margin-top:5px;
+	    width:330px;
+	    height:100px;
+	    overflow:auto;
+	}
+	.auto:hover {
+	    display:block;
+	    padding:5px;
+	    margin-top:5px;
+	    width:330px;
+	    height:100px;
+	    overflow:auto;
+	}
+</style>
 <body>
 	<thead>
 		<div class="logo">
@@ -120,43 +133,70 @@
 		</div>
 	</thead>
 	<tbody>
-		<div class="container-fluid" style="margin: 50px; padding:30px;
-	width: calc(100% - 100px);">
-		<h2>History Penggunaan</h2>
-		<form action="" method="post">
-			<input style="margin-left: 800px;" class="search" type="text" name="keyword" placeholder="Cari..." autocomplete="off" required>
-			<input class="button" type="submit" name="cari" value="Cari">
-		</form>
-		<hr style="position: relative; border: none; height: 1px; background: #999;" />
-		<table class="table text-center align-middle">
-				<tr>
-					<th>No.</th>
-					<th>Nama Alat</th>
-					<th>Waktu</th>
-					<th>Pengguna</th>
-				</tr>
+		<div class="container">
+			<div class="container-fluid" style="margin: 50px; padding:30px;
+			width: calc(100% - 100px);">
+					<h2>History Penggunaan</h2>
+						<div class="data-tables datatable-dark">
+							
+		                <table class="table table-borderes text-center align-middle" id="mauexport">
+						<thead>
+		                <tr>
+							<th>No.</th>
+							<th>Nama Alat</th>
+							<th>Waktu</th>
+							<th>Pengguna</th>
+						</tr>
+		                </thead>
+		                <tbody>
+						<?php $i = 1; ?>
+						<?php 
+							while ($row = mysqli_fetch_assoc($result)) : ?>
+							<tr>
+								<td><?=$i; ?></td>
+								<td><?= $row["nama_alat"] ?></td>
+								<td>
+									<div class="card border-primary">
+										<div class="card-body text-primary">
+											<p class="card-text"><?= $row["checkin"] ?>-<?= $row["checkout"] ?></p>
+										</div>
+									</div>
+								</td>
+								<td><?= $row["nama"] ?></td>
+							</tr>
+							<?php $i++; ?>
+						<?php 
+							//endforeach; 
+							endwhile;
+						?>
+		                </tbody>
+					</table>
+							
+						</div>
+		</div>
+			
+		<script>
+		$(document).ready(function() {
+		    $('#mauexport').DataTable( {
+		        dom: 'Bfrtip',
+		        buttons: [
+		            'excel', 'pdf', 'print'
+		        ]
+		    } );
+		} );
 
-				<?php $i = 1; ?>
-				<?php 
-					while ($row = mysqli_fetch_assoc($result)) : ?>
-					<tr>
-						<td><?=$i; ?></td>
-						<td><?= $row["nama_alat"] ?></td>
-						<td>
-							<div class="card border-primary" style="max-width: 21rem;">
-								<div class="card-body text-primary">
-									<p class="card-text"><?= $row["checkin"] ?>-<?= $row["checkout"] ?></p>
-								</div>
-							</div>
-						</td>
-						<td><?= $row["nama"] ?></td>
-					</tr>
-					<?php $i++; ?>
-				<?php 
-					//endforeach; 
-					endwhile;
-				?>
-			</table>
+		</script>
+
+		<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+		<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+		<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+		<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
 		</div>
 	</tbody>
 </body>

1efc8ebb0cb7b928512421b4d29a6ab426d4a31d
Rufrufina27 <agnesstefani30@gmail.com>
2021-11-19 14:30:09 +0700

    ulang_coba.php

--- Project2/coba.php
+++ Project2/coba.php
@@ -18,124 +18,23 @@
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </head>
-<style>
-	body{
-		font-family: sans-serif;
-		background-image: url(bg.jpg);
-		background-repeat: no-repeat;
-		background-size: cover;
-	}
-	.container-fluid{
-    	border-radius: 25px;
-    	background-color: rgba(255,255,255,0.8);
-    	box-shadow: 0 0 17px #333;
-	}
-	.home{
-		float: right;
-		display: inline-block;
-		width: 350px;
-		height: 50px;
-		list-style: none;
-		line-height: 50px;	
-		color: black;
-		text-align: center;
-		font-size: 20px;
-	}
-	.home img{
-		width: 30px;
-		height: 30px;
-	}
-	nav{
-		width: 50%;
-		height: 30px;
-		border : 0px solid;
-		line-height: 30px;
-		float: right;
-	}
-	nav ul li{
-		width: 20%;
-		height: 30px;
-		float: left;
-		list-style: none;
-		margin-bottom: 5px;
-	}
-	nav ul li:hover ul{
-		display: block;
-	}
-	nav ul li ul{
-		display: none;
-	}
-	nav ul li ul li{
-		width: 300px;
-		height: 40px;
-		background-color: #ffff;
-		margin-bottom: 2px;
-		border-style: solid !important;
-		text-align: left !important;
-	}
-	.auto {
-	    display:none;
-	    padding:5px;
-	    margin-top:5px;
-	    width:330px;
-	    height:100px;
-	    overflow:auto;
-	}
-	.auto:hover {
-	    display:block;
-	    padding:5px;
-	    margin-top:5px;
-	    width:330px;
-	    height:100px;
-	    overflow:auto;
-	}
-</style>
-<body>
-	<thead>
-		<div class="logo">
-			<img src="aset/Logonobg.png" width="140px" height="50px">
-			<nav>
-				<ul class="home">
-					<li>
-						<img src="gambar/notif.png">
-						<ul class="auto">
-							<?php 							
-							$datetime = new DateTime;
-							$otherTZ = new DateTimeZone("Asia/Jakarta");
-							$datetime->setTimezone($otherTZ);
-							$date = $datetime->format('Y-m-d');
 
-							$statement = $dbc->prepare("SELECT nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat AND DATE(kalibrasi.tgl_kalibrasi) >= :date order by kalibrasi.tgl_kalibrasi ASC");
-							$statement->execute(['date' => $date]);
-							$data = $statement->fetchAll();
-							foreach ($data as $row) {
-								$kalibrasi = new DateTime($row["tgl_kalibrasi"]);
-								$tgl = $kalibrasi->format("Y-m-d");
-								
-								echo "<a href=''><li>{$row['nama_alat']}</li></a>";
-								echo "<p>Lakukan kalibrasi sebelum {$tgl}</p>";
-							}
-							?>
-						</ul>
-					</li>
-					<a href="admin.php" style="margin-right: 30px">Home</a>
-					<a href="login.php">Logout</a>
-				</ul>
-			</nav>
-		</div>
-	</thead>
-	<tbody>
-		<div class="container-fluid" style="margin: 50px; padding:30px;
-		width: calc(100% - 100px);">
-		<h2>History Penggunaan</h2>
-		<div class="data-tables datatable-dark">	
-		    <table class="table table-borderes text-center align-middle" id="mauexport">
-		        <tr>
+<body>
+<div class="container-fluid" style="margin: 50px; padding:30px;
+	width: calc(100% - 100px);">
+			<h2>History Penggunaan</h2>
+				<div class="data-tables datatable-dark">
+					
+                <table class="table table-borderes text-center align-middle" id="mauexport">
+				<thead>
+                <tr>
 					<th>No.</th>
 					<th>Nama Alat</th>
 					<th>Waktu</th>
 					<th>Pengguna</th>
 				</tr>
+                </thead>
+                <tbody>
 				<?php $i = 1; ?>
 				<?php 
 					while ($row = mysqli_fetch_assoc($result)) : ?>
@@ -152,14 +51,15 @@
 						<td><?= $row["nama"] ?></td>
 					</tr>
 					<?php $i++; ?>
-					<?php 
-						//endforeach; 
-						endwhile;
-					?>
+				<?php 
+					//endforeach; 
+					endwhile;
+				?>
+                </tbody>
 			</table>
-		</div>
-		</div>
-	</tbody>
+					
+				</div>
+</div>
 	
 <script>
 $(document).ready(function() {

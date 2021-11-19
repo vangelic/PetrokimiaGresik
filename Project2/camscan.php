7585556043c19c44597a042003d5a9402b5b889b
vangelic <ivfatusy@gmail.com>
2021-11-19 15:11:31 +0700

    Update camscan.php

--- Project2/camscan.php
+++ Project2/camscan.php
@@ -22,7 +22,7 @@
 			  <div class="col-md-6">
 				  <video id="preview" width="100%"></video>
 			  </div>
-			  <div class="col-md-6">
+			  <div class="col-md-6 mt-3">
 			  <form method="POST">
 				  <label>SCAN QR CODE</label>
 				  <input type="text" name="text" id="text" readonyy="" placeholder="scan qrcode" class="form-control">

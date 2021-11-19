a2b2e588919123b7b1875aa99fa13795c23774fa
Rufrufina27 <agnesstefani30@gmail.com>
2021-11-19 13:41:50 +0700

    pgcode

--- Project2/pgcode.php
+++ Project2/pgcode.php
@@ -97,48 +97,12 @@
         line-height: 30px;
         float: right;
     }
-    nav ul li{
+    nav ul{
         width: 20%;
         height: 30px;
         float: left;
         list-style: none;
         margin-bottom: 5px;
-    }
-    nav ul li:hover ul .auto{
-        display: block;
-        border: 1px solid red;
-        padding:5px;
-        margin-top:5px;
-        width:300px;
-        height:300px;
-        overflow:auto;
-    }
-    nav ul li ul{
-        display: none;
-    }
-    nav ul li ul li{
-        width: 300px;
-        height: 40px;
-        background-color: #ffff;
-        margin-bottom: 2px;
-        border-style: solid !important;
-        text-align: left !important;
-    }
-    .auto {
-        display:none;
-        padding:5px;
-        margin-top:5px;
-        width:330px;
-        height:100px;
-        overflow:auto;
-    }
-    .auto:hover {
-        display:block;
-        padding:5px;
-        margin-top:5px;
-        width:330px;
-        height:100px;
-        overflow:auto;
     }
 </style>
 
@@ -148,28 +112,6 @@
             <img src="aset/Logonobg.png" width="140px" height="50px">
             <nav>
                 <ul class="home">
-                    <li>
-                        <img src="gambar/notif.png">
-                        <ul class="auto">
-                            <?php                           
-                            $datetime = new DateTime;
-                            $otherTZ = new DateTimeZone("Asia/Jakarta");
-                            $datetime->setTimezone($otherTZ);
-                            $date = $datetime->format('Y-m-d');
-
-                            $statement = $dbc->prepare("SELECT nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat AND DATE(kalibrasi.tgl_kalibrasi) >= :date order by kalibrasi.tgl_kalibrasi ASC");
-                            $statement->execute(['date' => $date]);
-                            $data = $statement->fetchAll();
-                            foreach ($data as $row) {
-                                $kalibrasi = new DateTime($row["tgl_kalibrasi"]);
-                                $tgl = $kalibrasi->format("Y-m-d");
-                                
-                                echo "<a href=''><li>{$row['nama_alat']}</li></a>";
-                                echo "<p>Lakukan kalibrasi sebelum {$tgl}</p>";
-                            }
-                            ?>
-                        </ul>
-                    </li>
                     <a href="admin.php" style="margin-right: 30px">Home</a>
                     <a href="login.php">Logout</a>
                 </ul>

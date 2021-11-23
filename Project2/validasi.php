21c545fd5d2319fe9468c576d111d527eae499e7
vangelic <ivfatusy@gmail.com>
2021-11-23 15:07:28 +0700

    Update validasi.php

--- Project2/validasi.php
+++ Project2/validasi.php
@@ -5,7 +5,9 @@ include "inisiasi.php";
 	$id = $_GET['id'];
 
 	try {
-		$db->update('daftar_alat', ['kondisi' => Null], ['nama_alat'=> $id]);
+		$result = mysqli_query($koneksi, "SELECT id_kalibrasi, kalibrasi.id_alat, nama_alat FROM `kalibrasi`, daftar_alat WHERE kalibrasi.id_alat = daftar_alat.id_alat AND id_kalibrasi=$id");
+		$row = mysqli_fetch_assoc($result);
+		$db->update('daftar_alat', ['kondisi' => Null], ['nama_alat'=> $row['nama_alat']]);
 
 		$statement = $dbc->prepare("DELETE FROM kalibrasi WHERE id_kalibrasi = :id");
 		$statement->bindValue(':id', $_GET['id']);

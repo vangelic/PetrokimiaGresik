bda72c8a814c78c3fe6e6585afad4cfb719a943b
vangelic <ivfatusy@gmail.com>
2021-11-23 13:41:28 +0700

    Update adminPermission.inc.php

--- Project2/adminPermission.inc.php
+++ Project2/adminPermission.inc.php
@@ -6,4 +6,16 @@
         header("Location: $url/login.php");
         exit();
     }
+
+    $waktu = new DateTime;
+	$zona = new DateTimeZone("Asia/Jakarta");
+	$waktu->setTimezone($zona);
+	$tgl = $waktu->format('Y-m-d');
+
+	$que = $dbc->prepare("SELECT nama_alat, tgl_kalibrasi FROM kalibrasi, daftar_alat WHERE kalibrasi.id_alat=daftar_alat.id_alat AND DATE(kalibrasi.tgl_kalibrasi) <= :date order by kalibrasi.tgl_kalibrasi ASC");
+	$que->execute(['date' => $tgl]);
+	$data = $que->fetchAll();
+    foreach ($data as $row) {
+		$db->update('daftar_alat', ['kondisi' => 'Rusak'], ['nama_alat'=> $row['nama_alat']]);
+	}
 ?>

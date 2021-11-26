b926b7a13e24db979a60a0a18d7d41da3f92776f
vangelic <ivfatusy@gmail.com>
2021-11-26 14:33:31 +0700

    Update daftar_alat.php

--- Project2/daftar_alat.php
+++ Project2/daftar_alat.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Status Alat</title>
+	<title>Daftar Alat</title>
 	<style>
 		body{
 		font-family: sans-serif;
@@ -187,20 +187,7 @@
 									<tr>
 										<td><?=$i; ?></td>
 										<td><?= $row["nama_alat"] ?></td>
-										<td>
-											<?php
-											if ($row["kondisi"] == "Rusak"){
-												echo "<a href=".$url."/validate.php?id=".$row['id_alat'].">";
-											?>
-												<button type="button" class="btn btn-primary">
-													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
-													<path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-													<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-													</svg>
-												</button>
-											</a>
-											<?php } ?>
-										</td>
+										<td></td>
 										<td>
 										<?php 
 

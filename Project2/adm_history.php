09e059ea058d2f6442a8f4e989642faf26736c9c
Rufrufina27 <agnesstefani30@gmail.com>
2021-12-01 13:58:18 +0700

    ulang

--- Project2/adm_history.php
+++ Project2/adm_history.php
@@ -25,44 +25,12 @@
 		background-repeat: no-repeat;
 		background-size: cover;
 	}
-	.header{
-		text-align: center;
-		padding-top: 50px;
-		margin-left: 0px;
-		padding-bottom: 50px;
-	}
 	.container{
-		width: 800px !important;
+		width: auto;
 		margin:5% auto;
 		border-radius: 25px;
-		background-color: rgba(255,255,255,0.5);
+		background-color: rgba(255,255,255,255);
 		box-shadow: 0 0 17px #333;
-	}
-	.main button{
-		padding-left: 0;
-		background-color:  #4b4e4d;
-		letter-spacing: 2px;
-		font-weight: bold;
-		width: 200px;
-		height: 200px;
-		margin-top: 10px;
-		margin-bottom: 30px;
-		border-radius: 15px ;
-		margin-left: 30px;
-		margin-right: 30px;
-	}
-	.main img{
-		width: 120px; 
-		height: 120px;
-		margin-bottom: 15px;
-	}
-	.main a{
-		color: white;
-		font-size: 20px;
-	}
-	.main button:hover{
-		box-shadow: 2px 5px 5px #555;
-		background-color: #8a8f8d;
 	}
 	.home{
 		float: right;

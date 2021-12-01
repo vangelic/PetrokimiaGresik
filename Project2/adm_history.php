f7620c70f634bf7c478cd2259b55ac34713d3c3a
Rufrufina27 <agnesstefani30@gmail.com>
2021-12-01 13:49:34 +0700

    notif adm_history.php

--- Project2/adm_history.php
+++ Project2/adm_history.php
@@ -130,7 +130,7 @@
 			<nav>
 				<ul class="home">
 					<li>
-						<img src="gambar/notif.png">
+						<img src="gambar/notification.png">
 						<ul class="auto">
 							<?php 							
 							$datetime = new DateTime;

650ee0cbd7c15edb0a277cca4f599198cf851d57
vangelic <ivfatusy@gmail.com>
2021-12-01 13:54:18 +0700

    Update cam_scan.php

--- Project2/cam_scan.php
+++ Project2/cam_scan.php
@@ -1,3 +1,21 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="jquery.min.js"></script>
 <script src="instascan.min.js"></script>
+
+<body>
+	<div class="container">
+		<div class="row justify-content-center mt-5">
+			<div class="col=md-5">
+				<div class="card-header bg-transparent mb-0">
+					<h5 class="text center">
+						<span class="font-weight-bold text-primary">Scan</span>
+					</h5>
+					<div class="card-body">
+
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>

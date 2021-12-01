a47c48901a73808388e672ebb83daf7c4447dadc
vangelic <ivfatusy@gmail.com>
2021-12-01 14:53:06 +0700

    Update camscan.php

--- Project2/camscan.php
+++ Project2/camscan.php
@@ -32,7 +32,7 @@
 	}
   </style>
   <body>
-	  <div class="container my-3">
+	  <div class="container py-3">
 		  <div class="row">
 			  <div class="col-md-6">
 				  <video id="preview" width="100%"></video>
